4c477a848bd010e88a296b9f48d368447a928f29
Varsha <hp@DESKTOP-4IENAI5>
2020-05-17 23:25:16 +0530

    FSJ0420A113

--- src/service/ExcelGenerator.java
+++ src/service/ExcelGenerator.java
@@ -1,13 +1,15 @@
 package service;
-/*
+
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 import java.util.List;
 
-import org.apache.poi.hssf.usermodel.HSSFRow;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.sl.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
 
 import model.Prograd;
 
@@ -17,14 +19,34 @@ import model.Prograd;
 //Stick to the instructions clearly. If you face any issue contact your mentor to get the guidance. 
 
 public class ExcelGenerator {
-	
+	String filename="G:\\Book1.xlsx";
+	int i=1;
 	FileOutputStream out;
-	public HSSFWorkbook excelGenerate(Prograd prograd, List<Prograd> list) throws IOException {
+	public Workbook excelGenerate(Prograd prograd, List<Prograd> list) throws IOException {
 		try {
 
-
+			Workbook hwb = new XSSFWorkbook();
+			Sheet sheet=(Sheet) hwb.createSheet("ProGradDetails");
+			Row row=((org.apache.poi.ss.usermodel.Sheet) sheet).createRow(0);
+			
+			row.createCell(0).setCellValue("ProGrad Name");
+			row.createCell(1).setCellValue("ProGrad Id");
+			row.createCell(2).setCellValue("ProGrad Rate");
+			row.createCell(3).setCellValue("ProGrad Comment");
+			row.createCell(4).setCellValue("ProGrad Recommend");
+			
+		 	
+			for(Prograd fillSheet: list) {
+	      	 
+	      	  Row nextRows = ((org.apache.poi.ss.usermodel.Sheet) sheet).createRow(i);
+	      	nextRows.createCell(0).setCellValue(fillSheet.getName());
+	      	nextRows.createCell(1).setCellValue(fillSheet.getId());
+	      	nextRows.createCell(2).setCellValue(fillSheet.getRate());
+	      	nextRows.createCell(3).setCellValue(fillSheet.getComment());
+	      	nextRows.createCell(4).setCellValue(fillSheet.getRecommend());
 			
 			// Type your code here
+			
 			
 		
 			}
@@ -44,4 +66,3 @@ public class ExcelGenerator {
 		
 	}
 }
-*/
